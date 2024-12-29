5be5fac44df8e2e06c0aa4ebc2c7dba88d296989
阿俊 <xiaojunma@vip.qq.com>
2024-12-29 17:42:04 +0800

    fix(build):修改workflows

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	a := app.New()
+	w := a.NewWindow("MyPC")
+
+	// 设置尺寸
+	// w.Resize(fyne.NewSize(500, 400))
+	// 边距填充
+	w.SetPadded(false)
+	// 是否禁止改变窗口尺寸
+	w.SetFixedSize(true)
+
+	hello := widget.NewLabel("Hello Fyne DDD")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
 }
